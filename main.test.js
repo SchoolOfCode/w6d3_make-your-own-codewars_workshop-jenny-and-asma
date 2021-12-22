42662844bacf09f3b4e4d5092ae374c1cc2b6af7
Jenny <jenny.m.lewis@hotmail.co.uk>
2021-12-22 16:34:08 +0000

    Some tests written and working

--- main.test.js
+++ main.test.js
@@ -1,20 +1,33 @@
 import catDemands from "./main.js";
+const catDemandsByTime = [
+  [10, `Feed me, you can see I'm hungry. My bowl is empty, I'm STARVING!`],
+  [11, `It's been a while since I ate, this litter tray is also starting to pong a bit.`],
+  [12, `Leave me alone, I'm taking nap!`],
+  [13, `Is that a chicken sandwich.`],
+  [14, `I'd like a belly rub... no wait, don't touch me.`],  
+  [15, `Get off your phone and pay attention to me.`],
+  [16, `Here I've brought you this dead thing.`],
+  [17, `Get your offspring away from me!`],
+  [18, `Hello! I'm very pleased to see you, allow me to give you a front row view of my bum.`],
+  [19, `I have a bet on how long it will take you to find the pee I did in your room.`],
+  [3, `I see dead people...it's time to play.`]
+]
 
 //👉 Write your tests below here:
 
 describe("The catDemands function", () => {
-  it(
-    `Should take 10:00 and return: "Feed me, you can see I'm hungry. My bowl is empty, I'm STARVING!"`
-  );
-
+  it.each(catDemandsByTime)(`Should take %i and return: %s`, (hour, string) => 
+    expect(catDemands(hour)).toBe(string)
+);
   // arrange
-  const time = "10.00";
+  const time = 19;
   const expected =
-    "Feed me, you can see I'm hungry. My bowl is empty, I'm STARVING!";
-
+  `I have a bet on how long it will take you to find the pee I did in your room.`;
   // act
   const actual = catDemands(time);
-
   // assert
   expect(actual).toBe(expected);
-});
+    }
+);
+
+//Test needed to check if triple equals is used as tests currently allow "= integer" of any kind to pass.
